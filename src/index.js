6dcc3250783d8a964d407807a7e20ef2cfb90af1
vertefra <verte.fra@gmail.com>
2020-09-04 18:05:01 -0400

    Browser router added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Dashboard from './components/Dashoboard';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(
+// every route will be added to the routes array
+
+const routes = [ 
+  {
+      path: '/',
+      component: Dashboard,
+      key: 'dashboard'
+  }  
+]
+
+ReactDOM.render( 
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {
+        routes.map(route => {
+          return <Route
+                    component={route.component}
+                    path={route.path}
+                    key={route.key}
+                  />
+        })
+      }
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 
-serviceWorker.unregister();
