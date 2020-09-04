4734e1aa54558238e84372108d54fbe64e5693ec
vertefra <verte.fra@gmail.com>
2020-09-04 18:41:52 -0400

    added store for user Context

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dashboard from './components/Dashoboard';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Dashboard from "./components/Dashoboard";
+import { BrowserRouter, Route } from "react-router-dom";
+import userState from "../stores/userState.js";
 
 // every route will be added to the routes array
 
-const routes = [ 
+const routes = [
   {
-      path: '/',
-      component: Dashboard,
-      key: 'dashboard'
-  }  
-]
+    path: "/",
+    component: Dashboard,
+    key: "dashboard",
+  },
+];
 
-ReactDOM.render( 
+// Here is where we create all the context for the states that we want
+
+const UserContext = React.createContext(userState);
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      {
-        routes.map(route => {
-          return <Route
-                    component={route.component}
-                    path={route.path}
-                    key={route.key}
-                  />
-        })
-      }
+      {routes.map((route) => {
+        return (
+          <Route
+            component={route.component}
+            path={route.path}
+            key={route.key}
+          />
+        );
+      })}
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
