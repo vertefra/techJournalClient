e8f7e796b0d18c7a58284d636e67e5795b0d839a
Maha Mahmood <>
2020-09-06 22:58:20 -0400

    added layout to events.js. started code for new event

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,13 +1,40 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../context/ContextStore";
+import Layout from "../layout/Layout";
+import axios from "axios";
 
 function Events(props) {
     const [userState, dispatchUserState] = useContext(UserContext);
-    console.log('events is working')
+    // const [newEvent, updateNewEvent] = useState({
+    //     title: '',
+    //     date: '',
+    //     time: '',
+    //     description: '',
+    //     location: '',
+    //     topic: '',
+    //     host: {},
+    //     speaker: {},
+    // });
+    // const handleCreateEvent = () => {
+    //     updateNewEvent({ ...newEvent, [event.target.id]: event.target.value });
+
+    //     const handlleCreate = async event => {
+    //         event.preventDefault();
+    //         try {
+    //             const response = await axios.post('http://localhost:3001/users/events', {
+
+    //             })
+    //         }
+    // }
+
+    // }
+
     return (
-        <div>
-            <h1>All Events</h1>
-        </div>
+        <Layout>
+            <div>
+                <h1>All Events Created by a User</h1>
+            </div>
+        </Layout>
     );
 
 }
