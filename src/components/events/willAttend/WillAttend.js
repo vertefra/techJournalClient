a849bb894279599dbb5fd1dc1dc56e7b8127babf
Maha Mahmood <>
2020-09-09 15:23:17 -0400

    started willAttend handle for the will attend button on the event show page

---
+++ src/components/events/willAttend/WillAttend.js
@@ -0,0 +1,39 @@
+import React, { useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import { server } from '../../../setting';
+import { UserContext } from '../../../context/ContextStore';
+
+
+function WillAttend(props) {
+    const [userState, dispatchUserState] = useContext(UserContext);
+    const [event, updateEvent] = useState({});
+    const [eventsWillAttend, updateEventsWillAttend] = useState({
+        eventsWillAttend: []
+    });
+
+    const handleEventsWillAttend = (event) => {
+        {
+            if (userState.loggedIn) {
+                (async () => {
+                    try {
+                        const response = await axios.post(`${server}/users/${userState.id}/events/${event.target.id}`)
+                        console.log(response);
+                        updateEventsWillAttend({ ...eventsWillAttend })
+                    } catch (error) {
+                        console.log(error);
+                    }
+                })();
+            } else {
+                console.log('please log in')
+            }
+        }
+    };
+
+    return (
+        <div>
+            <button id={event._id} onClick={handleEventsWillAttend}>Attend Event</button>
+        </div>
+    );
+};
+
+export default WillAttend;
