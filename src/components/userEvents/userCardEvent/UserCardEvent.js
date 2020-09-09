ec525f482d721e48ad4718312fe474a038b904a3
Maha Mahmood <>
2020-09-08 23:09:17 -0400

    started code for delete button

--- src/components/userEvents/userCardEvent/UserCardEvent.js
+++ src/components/userEvents/userCardEvent/UserCardEvent.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { UserContext } from "../../../context/ContextStore";
+import axios from "axios";
+import { server } from '../../../setting';
 
 function UserCardEvent(props) {
     const event = props.event;
-    console.log(event)
+    const [userState, dispatchUserState] = useContext(UserContext);
+    const [events, updateEvents] = useState({});
+    // console.log(event)
+
+    const handleDelete = (event) => {
+        (async () => {
+            try {
+                const response = await axios.delete(`${server}/users/${userState.id}/events/${event.target.id}`)
+                console.log(response);
+                updateEvents({ ...events });
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    };
+
+
     return (
         <div className="CardEventContainer">
             <div className="CardItem">
@@ -26,6 +45,7 @@ function UserCardEvent(props) {
                     <h4>Email: {event.host.email}</h4>
                     <h4>Phone Number: {event.host.phoneNumber}</h4>
                 </div>
+                <button id={event._id} onClick={handleDelete}>Delete</button>
             </div>
         </div>
     );
