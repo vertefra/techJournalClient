40046cc1f42324ef305c20961e8f671ff4dbfba7
Maha Mahmood <>
2020-09-10 11:54:54 -0400

    removed key from map.js

--- src/components/locationSearch/component/Map.js
+++ src/components/locationSearch/component/Map.js
@@ -3,7 +3,6 @@ import GoogleMapReact from "google-map-react";
 import MyLocationPin from "./MyLocationPin";
 import "./style/map.css";
 import EventsLocationPin from "./EventsLocationsPin";
-const key = "AIzaSyBpqB23w-8bt3yQP1cYL6zQU9wKz7j6SmI";
 
 export default function Map({ myLocation, zoomLevel, eventsLocations }) {
   return (
@@ -11,7 +10,7 @@ export default function Map({ myLocation, zoomLevel, eventsLocations }) {
       <div className="google-map">
         <GoogleMapReact
           style={{ height: "100%", width: "100%" }}
-          bootstrapURLKeys={{ key }}
+          // bootstrapURLKeys={{ key }}
           defaultCenter={myLocation}
           defaultZoom={zoomLevel}
           controlSize="400px"
