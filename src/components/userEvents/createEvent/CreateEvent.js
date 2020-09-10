a9a437d65b48388789dce06d10b0a8b00e4cab20
Maha Mahmood <>
2020-09-09 23:47:22 -0400

    added some style to create new event card

--- src/components/userEvents/createEvent/CreateEvent.js
+++ src/components/userEvents/createEvent/CreateEvent.js
@@ -71,122 +71,149 @@ function CreateEvent(props) {
     };
 
     return (
-        <div className="CardEventContainer">
-            <div className="CardItem">
-                <h1>Create a New Event</h1>
+        <div className="CreateEventContainer">
+            <div className="CreateEventCard">
+                <h1 className="MainFormHeadline">Create a New Event</h1>
                 <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor='title'>Title</label><br />
-                        <input
-                            type='text'
-                            name='title'
-                            id='title'
-                            value={event.title}
-                            onChange={handleChange}
-                        /><br />
+                    <div className="FormEvent">
+                        <div className="HostInfo">
+                            <h3 className="Legend">Host Information</h3>
+                            <div>
+                                <label htmlFor='hostName'>Host Name</label><br />
+                                <input
+                                    className="FormInput"
+                                    type='text'
+                                    name='hostName'
+                                    id='hostName'
+                                    value={host.name}
+                                    onChange={handleChangeHost}
+                                /><br />
+                            </div>
+                            <div>
+                                <label htmlFor='hostTitle'>Host Title</label><br />
+                                <input
+                                    className="FormInput"
+                                    type='text'
+                                    name='hostTitle'
+                                    id='hostTitle'
+                                    value={host.title}
+                                    onChange={handleChangeHost}
+                                /><br />
+                            </div>
+                            <div>
+                                <label htmlFor='hostEmail'>Host Email</label><br />
+                                <input
+                                    className="FormInput"
+                                    type='email'
+                                    name='hostEmail'
+                                    id='hostEmail'
+                                    value={host.email}
+                                    placeholder='example@gmail.com'
+                                    onChange={handleChangeHost}
+                                /><br />
+                            </div>
+                            <div>
+                                <label htmlFor='hostPhoneNumber'>Host Phone Number</label><br />
+                                <input
+                                    className="FormInput"
+                                    type='number'
+                                    name='hostPhoneNumber'
+                                    id='hostPhoneNumber'
+                                    value={host.phoneNumber}
+                                    onChange={handleChangeHost}
+                                /><br />
+                            </div>
+                        </div>
+                        <div className="AboutTheEvent">
+                            <h3 className="Legend">About The Event</h3>
+                            <div>
+                                <label htmlFor='title'>Title</label><br />
+                                <input
+                                    className="FormInput"
+                                    type='text'
+                                    name='title'
+                                    id='title'
+                                    value={event.title}
+                                    onChange={handleChange}
+                                /><br />
+                            </div>
+                            <div>
+                                <label htmlFor='topics'>Topic</label><br />
+                                <input
+                                    className="FormInput"
+                                    type='text'
+                                    name='topics'
+                                    id='topics'
+                                    value={topics.skill}
+                                    onChange={handleChangeTopics}
+                                /><br />
+                            </div>
+                            <div>
+                                <label htmlFor='description'>Description</label><br />
+                                <input
+                                    className="FormInput"
+                                    type='text'
+                                    name='description'
+                                    id='description'
+                                    value={event.description}
+                                    onChange={handleChange}
+                                /><br />
+                            </div>
+                        </div>
+                        <div className="SpeakerInfo">
+                            <h3 className="Legend">Speaker Information</h3>
+                            <div className="SpeakerName">
+                                <div>
+                                    <label htmlFor='speakerName'>Speaker Name</label><br />
+                                    <input
+                                        className="FormInput"
+                                        type='text'
+                                        name='speakerName'
+                                        id='speakerName'
+                                        value={speaker.Name}
+                                        onChange={handleChangeSpeaker}
+                                    /><br />
+                                </div>
+                                <div>
+                                    <label htmlFor='speakerTitle'>Speaker Title</label><br />
+                                    <input
+                                        className="FormInput"
+                                        type='text'
+                                        name='speakerTitle'
+                                        id='speakerTitle'
+                                        value={speaker.title}
+                                        onChange={handleChangeSpeaker}
+                                    /><br />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="EventDetails">
+                            <h3 className="Legend">Events Details</h3>
+                            <div>
+                                <lable htmlFor='date'>Date and Time</lable>
+                                <input
+                                    className="FormInput"
+                                    type='datetime-local'
+                                    name='date'
+                                    id='date'
+                                    value={event.date}
+                                    onChange={handleChange}
+                                /><br />
+                            </div>
+                            <div>
+                                <label htmlFor='location'>Location</label><br />
+                                <input
+                                    className="FormInput"
+                                    type='text'
+                                    name='location'
+                                    id='location'
+                                    value={event.location}
+                                    onChange={handleChange}
+                                /><br />
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <lable htmlFor='date'>Date and Time</lable>
-                        <input
-                            type='datetime-local'
-                            name='date'
-                            id='date'
-                            value={event.date}
-                            onChange={handleChange}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='description'>Description</label><br />
-                        <input
-                            type='text'
-                            name='description'
-                            id='description'
-                            value={event.description}
-                            onChange={handleChange}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='location'>Location</label><br />
-                        <input
-                            type='text'
-                            name='location'
-                            id='location'
-                            value={event.location}
-                            onChange={handleChange}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='topics'>Topics</label><br />
-                        <input
-                            type='text'
-                            name='topics'
-                            id='topics'
-                            value={topics.skill}
-                            onChange={handleChangeTopics}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='hostName'>Host Name</label><br />
-                        <input
-                            type='text'
-                            name='hostName'
-                            id='hostName'
-                            value={host.name}
-                            onChange={handleChangeHost}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='hostTitle'>Host Title</label><br />
-                        <input
-                            type='text'
-                            name='hostTitle'
-                            id='hostTitle'
-                            value={host.title}
-                            onChange={handleChangeHost}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='hostEmail'>Host Email</label><br />
-                        <input
-                            type='email'
-                            name='hostEmail'
-                            id='hostEmail'
-                            value={host.email}
-                            placeholder='example@gmail.com'
-                            onChange={handleChangeHost}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='hostPhoneNumber'>Host Phone Number</label><br />
-                        <input
-                            type='number'
-                            name='hostPhoneNumber'
-                            id='hostPhoneNumber'
-                            value={host.phoneNumber}
-                            onChange={handleChangeHost}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='speakerName'>Speaker Name</label><br />
-                        <input
-                            type='text'
-                            name='speakerName'
-                            id='speakerName'
-                            value={speaker.Name}
-                            onChange={handleChangeSpeaker}
-                        /><br />
-                    </div>
-                    <div>
-                        <label htmlFor='speakerTitle'>Speaker Title</label><br />
-                        <input
-                            type='text'
-                            name='speakerTitle'
-                            id='speakerTitle'
-                            value={speaker.title}
-                            onChange={handleChangeSpeaker}
-                        /><br />
-                    </div>
-                    <input type="submit" value="Create a New Event" />
+                    <input className="btn-CreateEvent" type="submit" value="Create a New Event" />
                 </form>
             </div>
         </div>
