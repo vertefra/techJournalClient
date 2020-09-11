dfd90d3aa692f0bceeb60af211f3fb7eadf68bf3
vertefra <verte.fra@gmail.com>
2020-09-10 21:43:44 -0400

    readme

--- src/components/events/willAttend/WillAttend.js
+++ src/components/events/willAttend/WillAttend.js
@@ -1,40 +1,68 @@
-import React, { useContext, useState, useEffect } from 'react';
-import axios from 'axios';
-import { server } from '../../../setting';
-import { UserContext } from '../../../context/ContextStore';
-
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
+import { server } from "../../../setting";
+import { UserContext } from "../../../context/ContextStore";
 
 function WillAttend(props) {
-    const [userState, dispatchUserState] = useContext(UserContext);
-    const [event, updateEvent] = useState({ ...props.event });
-    const [eventsWillAttend, updateEventsWillAttend] = useState({
-        eventsWillAttend: []
+  const [userState] = useContext(UserContext);
+  const [event] = useState({ ...props.event });
+  const [isAttended, setIsAttended] = useState("");
+  const [eventsWillAttend, updateEventsWillAttend] = useState([]);
+
+  const handleEventsWillAttend = (event) => {
+    {
+      if (userState.loggedIn && isAttended === false) {
+        (async () => {
+          try {
+            const response = await axios.post(
+              `${server}/users/${userState.id}/events/${event.target.id}`
+            );
+            console.log(response);
+            // updateEventsWillAttend({ ...eventsWillAttend });
+          } catch (error) {
+            console.log(error);
+          }
+        })();
+      } else {
+        console.log("please log in");
+      }
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(
+          `${server}/users/${userState.id}/events?events=eventsWillAttend`
+        );
+        updateEventsWillAttend([...response.data]);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    const thisEvent = eventsWillAttend.find((evt) => {
+      return evt._id.toString() === event._id;
     });
+    if (thisEvent) {
+      setIsAttended(true);
+    }
+    console.log(thisEvent);
+  }, [eventsWillAttend]);
 
-    const handleEventsWillAttend = (event) => {
-        console.log(event.target.id)
-        {
-            if (userState.loggedIn) {
-                (async () => {
-                    try {
-                        const response = await axios.post(`${server}/users/${userState.id}/events/${event.target.id}`)
-                        console.log(response);
-                        updateEventsWillAttend({ ...eventsWillAttend })
-                    } catch (error) {
-                        console.log(error);
-                    }
-                })();
-            } else {
-                console.log('please log in')
-            }
-        }
-    };
-
-    return (
-        <div>
-            <button className="btn-WillAttend" id={event._id} onClick={handleEventsWillAttend}>Attend Event</button>
-        </div>
-    );
-};
+  return (
+    <div>
+      <button
+        className="btn-WillAttend"
+        id={event._id}
+        onClick={handleEventsWillAttend}
+      >
+        {isAttended ? "Attending" : "Attend Event"}
+      </button>
+    </div>
+  );
+}
 
 export default WillAttend;
