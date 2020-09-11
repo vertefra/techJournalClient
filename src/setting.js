e20bf6fe94121162711daddeb0b58e6f3f77c03f
vertefra <verte.fra@gmail.com>
2020-09-10 21:49:27 -0400

    prod

--- src/setting.js
+++ src/setting.js
@@ -1,6 +1,6 @@
 const dev = "http://127.0.0.1:3001";
 const prod = "https://techjournalserver.herokuapp.com";
-const server = dev;
+const server = prod;
 const dateFormat = {
   weekday: "long",
   month: "long",
